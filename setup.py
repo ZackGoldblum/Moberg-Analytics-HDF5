2fb2a1d3ed4a9593b956da48d6a8c286e41a0559
ZackGoldblum <zackgoldblum@gmail.com>
2021-03-08 10:24:35 -0500

    setup.pu update
    Removed "os" from install_requires.

--- setup.py
+++ setup.py
@@ -26,5 +26,5 @@ setup(
     #packages=find_packages(),
     packages=["moberg_analytics_hdf5"],
     include_package_data=True,
-    install_requires=["h5py", "numpy", "pandas", "os"]
+    install_requires=["h5py", "numpy", "pandas"]
 )
