e40212169575a26a495e4d89d005a183e6aaeb07
ZackGoldblum <zackgoldblum@gmail.com>
2021-03-08 14:29:07 -0500

    Version 1.0.3

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="Moberg-Analytics-HDF5",
-    version="1.0.2",
+    version="1.0.3",
     description="Moberg Analytics HDF5 Functions",
     long_description=README,
     long_description_content_type="text/markdown",
