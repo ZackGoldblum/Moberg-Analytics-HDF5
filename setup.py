b5bf84157e96619a2d1622db0d8f336277b0d562
ZackGoldblum <zackgoldblum@gmail.com>
2021-03-16 08:59:38 -0400

    Official PyPI Version

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 import pathlib
-from setuptools import setup, find_packages
+from setuptools import setup
 
 # directory containing this file
 HERE = pathlib.Path(__file__).parent
@@ -9,21 +9,19 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="Moberg-Analytics-HDF5",
-    version="1.0.6",
+    version="1.0.0",
     description="Moberg Analytics HDF5 Functions",
     long_description=README,
     long_description_content_type="text/markdown",
     url="https://github.com/Moberg-Analytics-Inc/Moberg-Analytics-HDF5",
     author="Moberg Analytics",
-    author_email="dick@moberganalytics.com",
+    author_email="zack.goldblum@moberganalytics.com",
     license="MIT",
     classifiers=[
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.9",
     ],
-    #packages=["moberg-hdf5", "hdf5_exceptions"],  # incorrect - files, not packages (folders)
-    #packages=find_packages(),
     packages=["moberg_analytics_hdf5"],
     include_package_data=True,
     install_requires=["h5py", "numpy", "pandas"]
