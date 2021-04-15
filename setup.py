4579e642c58517eecbdc64a477d93c3be0e8ac3c
ZackGoldblum <zackgoldblum@gmail.com>
2021-04-15 11:14:38 -0400

    removed files

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="Moberg-Analytics-HDF5",
-    version="1.0.7",
+    version="1.0.8",
     description="Moberg Analytics HDF5 Functions",
     long_description=README,
     long_description_content_type="text/markdown",
