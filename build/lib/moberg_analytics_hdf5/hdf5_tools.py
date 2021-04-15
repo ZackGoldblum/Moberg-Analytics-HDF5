0eb0a5f39227667c6c6f84cb00711830f7c06943
ZackGoldblum <zackgoldblum@gmail.com>
2021-04-15 11:27:02 -0400

    bump

--- build/lib/moberg_analytics_hdf5/hdf5_tools.py
+++ build/lib/moberg_analytics_hdf5/hdf5_tools.py
@@ -1,8 +1,8 @@
 import h5py
 import numpy as np
 import pandas as pd
-import os
-from . import hdf5_exceptions
+import ntpath
+import hdf5_exceptions
 
 
 class HDF5Helper:
@@ -304,7 +304,7 @@ class HDF5Content(HDF5Helper):
         """
         DESCRIPTION
         -----------
-        Returns a list of all group paths in the HDF5 file (including the Root group and subgroups).
+        Returns a list of all group paths in the HDF5 file (including the Root Group and subgroups).
         
         RETURNS
         -------
@@ -313,7 +313,7 @@ class HDF5Content(HDF5Helper):
         """
 
         all_group_paths_list = []
-        all_group_paths_list.append("/")  # add Root group to list
+        all_group_paths_list.append("/")  # add Root Group to list
 
         def visit_group(path):
             if isinstance(self.hdf5file[path], h5py._hl.group.Group):  # if group
@@ -327,7 +327,7 @@ class HDF5Content(HDF5Helper):
         """
         DESCRIPTION
         -----------
-        Returns a list of all group class objects in the HDF5 file (including the Root group and subgroups).
+        Returns a list of all group class objects in the HDF5 file (including the Root Group and subgroups).
         
         RETURNS
         -------
@@ -348,7 +348,7 @@ class HDF5Content(HDF5Helper):
         """
         DESCRIPTION
         -----------
-        Returns a list of all group names in the HDF5 file (including the Root group and subgroups).
+        Returns a list of all group names in the HDF5 file (including the Root Group and subgroups).
         
         RETURNS
         -------
@@ -362,7 +362,7 @@ class HDF5Content(HDF5Helper):
                 all_group_names_list.append(path.split("/")[-1])  # add group name to list
 
         self.hdf5file.visit(visit_group) # visit all objects in the HDF5 file
-        all_group_names_list.insert(0, "/")  # add Root group to start of list
+        all_group_names_list.insert(0, "/")  # add Root Group to start of list
 
         return all_group_names_list
 
@@ -370,7 +370,7 @@ class HDF5Content(HDF5Helper):
         """
         DESCRIPTION
         -----------
-        Returns a dictionary of all group names (including the Root group and subgroups) and their associated info from the HDF5 file.
+        Returns a dictionary of all group names (including the Root Group and subgroups) and their associated info from the HDF5 file.
             Key - group name
             Value - group info
         
@@ -423,7 +423,7 @@ class HDF5Content(HDF5Helper):
         """
         DESCRIPTION
         -----------
-        Returns a dictionary of all group objects (including the Root group and subgroups) and their group name from the HDF5 file.
+        Returns a dictionary of all group objects (including the Root Group and subgroups) and their group name from the HDF5 file.
             Key - group class object
             Value - group name
         
@@ -831,7 +831,7 @@ class HDF5Content(HDF5Helper):
             name of the HDF5 file
         """
 
-        hdf5_filename = os.path.basename(hdf5_filepath)  # works on Windows and Linux
+        hdf5_filename = ntpath.basename(hdf5_filepath)  # works on Windows and Linux
 
         return hdf5_filename
 
@@ -839,7 +839,7 @@ class HDF5Content(HDF5Helper):
         """
         DESCRIPTION
         -----------
-        Returns the group or dataset name from a HDF5 file path.
+        Returns the group or dataset name from the HDF5 file path.
 
         PARAMETERS
         ----------
@@ -876,8 +876,6 @@ class HDF5Content(HDF5Helper):
 
         if "/" in hdf5_path:  # if components in path
             split_path = hdf5_path.split("/")  # create list of components
-            if split_path[0] == "":
-                split_path = split_path[1:]
         else:  # if no components in path
             split_path = [hdf5_path]  # create list of the components
 
@@ -888,8 +886,8 @@ class HDF5Components(HDF5Content):
     """
     DESCRIPTION
     -----------
-    This class contains methods that return various components of the HDF5 file such as groups, 
-    datasets, dataset values, NumPy/Pandas matrices of dataset values, metadata, and structured dictionaries.
+    This class contains methods that return various components of the HDF5 file to the user.
+        Groups, datasets, dataset values, NumPy/Pandas matrices of dataset values, metadata, and structured dictionaries.
     
     PARAMETERS
     ----------
@@ -984,6 +982,7 @@ class HDF5Components(HDF5Content):
         except hdf5_exceptions.GroupNameError as e:
             print(e)
 
+    # TODO: test how this handles two levels of subgroups
     def get_group_dict(self, group_name):
         """
         DESCRIPTION
@@ -1062,6 +1061,45 @@ class HDF5Components(HDF5Content):
         except hdf5_exceptions.PathError as e:
             print(e)
 
+    def get_all_subgroup_dataset_names(self, group_name):
+        """
+        DESCRIPTION
+        -----------
+        Returns a list of all dataset names from the subgroups within the "group_name" group.
+        This is a list of only dataset names within the subgroups, it does not include dataset names in the "group_name" group itself.
+
+        PARAMETERS
+        ----------
+        group_name: str
+            name of the group that contains the subgroups
+            
+        RETURNS
+        -------
+        subgroup_dataset_names: list
+            list of the dataset names within the subgroups 
+        """
+
+        try:
+            valid_group_name = self.check_group_name(group_names_list=self.get_all_group_names(), group_name=group_name)
+            if valid_group_name:
+                subgroup_dict = self.get_group_dict(group_name=group_name)["subgroup_dict"]
+                subgroup_names_list = list(subgroup_dict.keys())  # list of subgroup names
+
+                if not subgroup_names_list:  # if there are no subgroups within the group
+                    print('There are no subgroups within this group.')
+
+                subgroup_dataset_names = []
+
+                for subgroup_name in subgroup_names_list:  # for each subgroup name
+                    dataset_dict = subgroup_dict[subgroup_name]["datasets"]
+                    dataset_names_list = list(dataset_dict.keys())  # list of dataset names in the subgroup_name subgroup
+                    subgroup_dataset_names.extend(dataset_names_list)  # add list of dataset names to the list
+
+                return subgroup_dataset_names
+
+        except hdf5_exceptions.GroupNameError as e:
+            print(e)
+
     def get_parent_group_obj(self, group_name=None, dataset_name=None, dataset_path=None):
         """
         DESCRIPTION
@@ -1092,7 +1130,7 @@ class HDF5Components(HDF5Content):
             if group_name:  # if group_name passed in
                 valid_group_name = self.check_group_name(group_names_list=self.get_all_group_names(), group_name=group_name)
                 if valid_group_name:
-                    if group_name != "/":  # if not the Root group
+                    if group_name != "/":  # if not the Root Group
                         group_obj = self.get_group_obj(group_name=group_name)
                         parent_group_obj = group_obj.parent  
                     else:
@@ -1151,7 +1189,7 @@ class HDF5Components(HDF5Content):
             if group_name:  # if group_name passed in
                 valid_group_name = self.check_group_name(group_names_list=self.get_all_group_names(), group_name=group_name)
                 if valid_group_name:
-                    if group_name != "/":  # if not the Root group
+                    if group_name != "/":  # if not the Root Group
                         group_obj = self.get_group_obj(group_name=group_name)
                         parent_group_path = group_obj.parent.name
                     else:
@@ -1212,11 +1250,11 @@ class HDF5Components(HDF5Content):
             if group_name:  # if group_name passed in
                 valid_group_name = self.check_group_name(group_names_list=self.get_all_group_names(), group_name=group_name)
                 if valid_group_name:
-                    if group_name != "/":  # if not the Root group
+                    if group_name != "/":  # if not the Root Group
                         parent_group_obj = self.get_parent_group_obj(group_name=group_name)
                         parent_group_name = all_group_objs_dict[parent_group_obj]["group_name"]
                     else:
-                        parent_group_name = None  # Root group "/" has no parent group name
+                        parent_group_name = None  # root group "/" has no parent group name
                 return parent_group_name
             elif dataset_name:  # if dataset_name passed in
                 valid_dataset_name = self.check_dataset_name(dataset_names_list=self.get_all_dataset_names(), dataset_name=dataset_name)
@@ -1542,22 +1580,42 @@ class HDF5Components(HDF5Content):
 
         try:
             if dataset_name:  # if dataset_name passed in
-                valid_dataset_name = self.check_dataset_name(dataset_names_list=self.get_all_dataset_names(), dataset_name=dataset_name)
-                if valid_dataset_name:
-                    dataset_path = self.get_path(dataset_name=dataset_name)
-                    dataset_obj = self.get_dataset_obj(dataset_path=dataset_path)
-                    if matrix_type == "pandas":
-                        column_names_list = self.get_column_names(dataset_path=dataset_path)  # list of dataset column names
-                        if column_names_list:  # if there are column names
-                            dataset_values = pd.DataFrame(data=dataset_obj[:], columns=column_names_list)  # Pandas DataFrame of dataset values
-                        else:  # if there are no column names
-                            dataset_values = pd.DataFrame(data=dataset_obj[:])  # Pandas DataFrame of dataset values
-                        return dataset_values
-                    elif matrix_type == "numpy":
-                        dataset_values = np.array(dataset_obj)  # NumPy Array of dataset values
-                        return dataset_values
-                    else:
-                        raise hdf5_exceptions.MatrixTypeError(matrix_type=matrix_type)
+                if isinstance(dataset_name, list):  # if dataset_name is a list of dataset names
+                    dataset_values_dict = {}
+                    for dataset_name_i in dataset_name:
+                        valid_dataset_name = self.check_dataset_name(dataset_names_list=self.get_all_dataset_names(), dataset_name=dataset_name_i)
+                        if valid_dataset_name:
+                            dataset_path = self.get_path(dataset_name=dataset_name_i)
+                            dataset_obj = self.get_dataset_obj(dataset_path=dataset_path)
+                            if matrix_type == "pandas":
+                                column_names_list = self.get_column_names(dataset_path=dataset_path)  # list of dataset column names
+                                if column_names_list:  # if there are column names
+                                    dataset_values = pd.DataFrame(data=dataset_obj[:], columns=column_names_list)  # Pandas DataFrame of dataset values
+                                else:  # if there are no column names
+                                    dataset_values = pd.DataFrame(data=dataset_obj[:])  # Pandas DataFrame of dataset values
+                            elif matrix_type == "numpy":
+                                dataset_values = np.array(dataset_obj)  # NumPy Array of dataset values
+                            else:
+                                raise hdf5_exceptions.MatrixTypeError(matrix_type=matrix_type)
+                            dataset_values_dict.update({dataset_name_i: dataset_values})
+                    return dataset_values_dict
+                elif isinstance(dataset_name, str):  # if dataset_name is a dataset name string
+                    valid_dataset_name = self.check_dataset_name(dataset_names_list=self.get_all_dataset_names(), dataset_name=dataset_name)
+                    if valid_dataset_name:
+                        dataset_path = self.get_path(dataset_name=dataset_name)
+                        dataset_obj = self.get_dataset_obj(dataset_path=dataset_path)
+                        if matrix_type == "pandas":
+                            column_names_list = self.get_column_names(dataset_path=dataset_path)  # list of dataset column names
+                            if column_names_list:  # if there are column names
+                                dataset_values = pd.DataFrame(data=dataset_obj[:], columns=column_names_list)  # Pandas DataFrame of dataset values
+                            else:  # if there are no column names
+                                dataset_values = pd.DataFrame(data=dataset_obj[:])  # Pandas DataFrame of dataset values
+                            return dataset_values
+                        elif matrix_type == "numpy":
+                            dataset_values = np.array(dataset_obj)  # NumPy Array of dataset values
+                            return dataset_values
+                        else:
+                            raise hdf5_exceptions.MatrixTypeError(matrix_type=matrix_type)
             elif dataset_path:  # if dataset_path passed in
                 valid_dataset_path = self.check_path(path_list=self.get_all_dataset_paths(), path_to_check=dataset_path)
                 if valid_dataset_path:
