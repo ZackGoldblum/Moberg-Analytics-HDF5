c6a520d9fbb09b77d6c5df1a857a2b6c0e5065a4
ZackGoldblum <zackgoldblum@gmail.com>
2021-04-15 10:58:26 -0400

    Version bump for Test PyPI

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="Moberg-Analytics-HDF5",
-    version="1.0.1",
+    version="1.0.6",
     description="Moberg Analytics HDF5 Functions",
     long_description=README,
     long_description_content_type="text/markdown",
